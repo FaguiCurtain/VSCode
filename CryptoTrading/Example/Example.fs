8c06d806babe10d948b4c38b637d0f870a04b45d
François-Guillaume RIDEAU <fgrideau@gmail.com>
2017-09-13 00:17:40 +0900

    updated example and references

--- CryptoTrading/Example/Example.fs
+++ CryptoTrading/Example/Example.fs
@@ -1,6 +1,19 @@
 module Example
+open Deedle
+open System
+
+type Price = {Day: DateTime; Open: float}
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
+    let x = "hello world from F#"
+    printfn "%A" x
+
+    let prices = 
+      [ { Day = DateTime.Now ; Open = 10.1}
+        { Day = DateTime.Now.AddDays(1.0); Open = 15.1}
+        { Day = DateTime.Now.AddDays(2.0); Open =  9.1} ]
+    let df = Frame.ofRecords prices
+    printfn "%A" df
+    // printfn "%A" argv
     0 // return an integer exit code
